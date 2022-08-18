a27ce200c69eb76b43d18957ae4a6e8351370991
hakkicansengonul <coder.9734@gmail.com>
2022-08-18 20:34:45 +0300

    Initial Commit

---
+++ Bankey-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey-Swift
+//
+//  Created by hakkı can şengönül on 18.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
